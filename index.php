f260ec4958a3131e236ef27446186bc227998803
manindra tamang <manindratamang5@gmai.com>
2022-06-03 15:45:39 +0545

    Ascii Art index

---
+++ index.php
@@ -0,0 +1,17 @@
+<title>Manindra Tamang PHP</title>
+<h1> Manindra Tamang php</h1>
+<body>
+
+  <pre>Ascii art:
+        **         **
+        ** **   ** **
+        **    **   **
+        **         **
+        **         **
+        **         **
+  </pre>
+<?php
+echo "The SHA256 hash of Manindra Tamang is ";
+print hash('sha256', 'Manindra Tamang PHP');
+ ?>
+ </body
